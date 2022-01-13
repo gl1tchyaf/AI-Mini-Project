3f4d731f5ebe21e63a051c95f7babf8c5dbab8e1
Hasib Ar Rafiul Fahim <85202959+gl1tchyaf@users.noreply.github.com>
2022-01-13 18:31:31 +0600

    Add files via upload

---
+++ 3_alt.py
@@ -0,0 +1,129 @@
+import math
+from simpleai.search import SearchProblem, astar
+
+
+# this methode will solve the maze
+class MazeSolver(SearchProblem):
+    def __init__(self, board):
+        self.board = board
+        self.goal = (0, 0)
+
+        for y in range(len(self.board)):
+            for x in range(len(self.board[y])):
+                if self.board[y][x].lower() == "o":
+                    self.initial = (x, y)
+                elif self.board[y][x].lower() == "x":
+                    self.goal = (x, y)
+
+        super(MazeSolver, self).__init__(initial_state=self.initial)
+
+    # searching the solution
+    def actions(self, state):
+        actions = []
+        for action in COSTS.keys():
+            newx, newy = self.result(state, action)
+            if self.board[newy][newx] != "#":
+                actions.append(action)
+
+        return actions
+
+    # Update maze location
+    def result(self, state, action):
+        x, y = state
+
+        if action.count("up"):
+            y -= 1
+        if action.count("down"):
+            y += 1
+        if action.count("left"):
+            x -= 1
+        if action.count("right"):
+            x += 1
+
+        new_state = (x, y)
+
+        return new_state
+
+    # Check if we have reached the goal
+    def is_goal(self, state):
+        return state == self.goal
+
+    # cost calculation
+    def cost(self, state, action, state2):
+        return COSTS[action]
+
+    # Heuristic values
+    def heuristic(self, state):
+        x, y = state
+        gx, gy = self.goal
+
+        return math.sqrt((x - gx) ** 2 + (y - gy) ** 2)
+
+
+# converting the maze
+def convert(MAP):
+    return [list(x) for x in MAP.split("\n") if x]
+
+
+# printing the maze
+def printMaze(MAP):
+    for y in range(len(MAP)):
+        for x in range(len(MAP[y])):
+            if (x, y) == problem.initial:
+                print('o', end='')
+            elif (x, y) == problem.goal:
+                print('x', end='')
+            elif (x, y) in path:
+                print('·', end='')
+            else:
+                print(MAP[y][x], end='')
+
+        print()
+
+
+# here is the map
+MAP = """
+    ##############################
+    #         #              #   #
+    # ####    ########       #   #
+    #  o #    #              #   #
+    #    ###     #####  ######   #
+    #      #   ###   #           #
+    #      #     #   #  #  #   ###
+    #     #####    #    #  # x   #
+    #              #       #     #
+    ##############################
+    """
+
+# Convert map to a list
+print(MAP)
+MAP = convert(MAP)
+
+# given movement cost
+cost_regular = 1.0
+cost_diagonal = 1.7
+
+# Create the cost dictionary
+COSTS = {
+    "up": cost_regular,
+    "down": cost_regular,
+    "left": cost_regular,
+    "right": cost_regular,
+    "up left": cost_diagonal,
+    "up right": cost_diagonal,
+    "down left": cost_diagonal,
+    "down right": cost_diagonal,
+}
+
+# Create maze solver object
+problem = MazeSolver(MAP)
+
+# Run the solver
+result = astar(problem, graph_search=True)
+
+# Extract the path
+path = [x[1] for x in result.path()]
+
+# Print the result
+print()
+printMaze(MAP)
